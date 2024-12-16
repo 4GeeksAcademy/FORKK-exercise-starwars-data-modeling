ed3b184add8f2a59cd713d8bb38f3b907fc37486
Ricard0-A <ricardoarias4@hotmail.com>
2024-12-16 13:21:16 +0000

    done

--- src/models.py
+++ src/models.py
@@ -1,17 +1,17 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Table
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-# Tabla intermedia para la relación muchos-a-muchos entre Usuario y Favoritos
-# Favoritos puede ser tanto planetas como personajes
+
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
 from sqlalchemy.orm import relationship
 
+# Todos apuntan a...
 class Favorito(Base):
     __tablename__ = 'favorito'
     id = Column(Integer, primary_key=True)
@@ -19,11 +19,8 @@ class Favorito(Base):
     planeta_id = Column(Integer, ForeignKey('planeta.id'), nullable=True)
     personaje_id = Column(Integer, ForeignKey('personaje.id'), nullable=True)
 
-    # Relación con la tabla Usuario
     usuario = relationship('Usuario', back_populates='favoritos')
-    # Relación con la tabla Planeta
     planeta = relationship('Planeta', back_populates='favoritos')
-    # Relación con la tabla Personaje
     personaje = relationship('Personaje', back_populates='favoritos')
 
 # Tabla Usuario
@@ -34,7 +31,6 @@ class Usuario(Base):
     password = Column(String(100), nullable=False)
     nombre = Column(String(50), nullable=False)
     apellido = Column(String(50), nullable=False)
-    fecha_subscripcion = Column(DateTime, nullable=False, default=func.now()) #Default para DateTime
     # Relación con la tabla Favorito
     favoritos = relationship('Favorito', back_populates='usuario')
 
@@ -46,7 +42,7 @@ class Planeta(Base):
     clima = Column(String(250), nullable=False)
     terreno = Column(String(250), nullable=False)
 
-    # Relación inversa con la tabla Favorito
+    # "Tab" con la tabla Favorito
     favoritos = relationship('Favorito', back_populates='planeta')
 
 # Tabla Personaje
@@ -58,9 +54,8 @@ class Personaje(Base):
     altura = Column(String(50), nullable=True)
     peso = Column(String(50), nullable=True)
 
-    # Relación inversa con la tabla Favorito
+    # "Tab" con la tabla Favorito
     favoritos = relationship('Favorito', back_populates='personaje')
 
 
-# Render del diagrama
 render_er(Base, 'diagram.png')
